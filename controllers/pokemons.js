45646028d221516754ffe7d68b4c741aef059008
Hermann Marx <marxhermann@web.de>
2021-02-08 17:56:21 +0100

    Implement getInfo

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,8 +1,22 @@
-const pokeData = require("../pokedex.json");
+const pokeData = require('../pokedex.json');
 
 const pokeController = {
   getAll: async (req, res) => {
-    res.send("Hello Pikachu");
+    res.send(pokeData);
+  },
+  getInfo: async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+
+    try {
+      const pokeInfo = pokeData.filter(pokemon => pokemon.id === id);
+
+      console.log(pokeInfo);
+      res.send('Testing');
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(404);
+    }
   },
 };
 
