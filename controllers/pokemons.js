030086927b978c46c49a58ba6c846aa2a9a32726
klarmann1 <66319208+klarmann1@users.noreply.github.com>
2021-02-09 16:37:55 +0100

    added getById and /:id route (#2)
    * added getById and /:id route
    
    * changed index to item, added status

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -1,8 +1,13 @@
-const pokeData = require("../pokedex.json");
+const pokeData = require('../pokedex.json');
 
 const pokeController = {
   getAll: async (req, res) => {
     res.status(200).send(pokeData);
+  },
+  getById: async (req, res) => {
+    const { id } = req.params;
+    const pokemon = pokeData.filter(item => item.id == id);
+    res.status(200).send(pokemon);
   },
 };
 
