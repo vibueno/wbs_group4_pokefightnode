33df98e40d5a68c8f7def49a10f743e2bea09ab8
HermannMarx <74415868+HermannMarx@users.noreply.github.com>
2021-02-09 16:47:56 +0100

    Get info (#3)
    * Implement getInfo
    
    * added router (/:id/:info) and specific middleware for it in controllerfile
    
    * added res.status(200)
    
    Co-authored-by: klarmann1 <66319208+klarmann1@users.noreply.github.com>

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -2,6 +2,7 @@ const pokeData = require('../pokedex.json');
 
 const pokeController = {
   getAll: async (req, res) => {
+
     res.status(200).send(pokeData);
   },
   getById: async (req, res) => {
@@ -9,6 +10,17 @@ const pokeController = {
     const pokemon = pokeData.filter(item => item.id == id);
     res.status(200).send(pokemon);
   },
-};
+  getInfo: async (req, res) => {
+    const { id, info } = req.params;
 
+    try {
+      const pokeId = pokeData.filter(pokemon => pokemon.id == id);
+      if (!pokeId[0][info]) res.sendStatus(404);
+      else res.status(200).send(pokeId[0][info]);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(404);
+    }
+  },
+};
 module.exports = pokeController;
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const pokeController = require('../controllers/pokemons');
 
 
+
 // show all users
+router.get('/:id/:info', pokeController.getInfo);
 router.get('/:id', pokeController.getById);
 router.get('/', pokeController.getAll);
+
 
 
 module.exports = router;
