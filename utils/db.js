5db082fafa2ec0c229a720b52a70eca04a8aa573
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-15 21:41:28 +0100

    Connect and get data from DB

---
+++ utils/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const { PORT, DBUSER, DBPASS, DBHOST, DBNAME } = process.env;
+
+const mongoDB = `mongodb+srv://${DBUSER}:${DBPASS}@${DBHOST}/${DBNAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
