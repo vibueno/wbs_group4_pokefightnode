7cf84c300620b4a249de51becc787b22b9004477
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-17 16:11:48 +0100

    Add validation for pokemon & winner

--- models/PokemonFight.js
+++ models/PokemonFight.js
@@ -6,55 +6,65 @@ const { validWinners, isSet } = require('../utils/validations');
 const pokemonFightSchema = new Schema({
   date: { type: Date, default: Date.now() },
   pokemon1: {
-    id: {
-      type: Number,
-      required: function (value) {
-        return isSet(value);
-      },
-    },
-    name: {
-      type: String,
-      required: function (value) {
-        return isSet(value);
-      },
-    },
-    winner: {
-      type: Boolean,
-      required: function (value) {
-        return isSet(value);
-      },
-      validate: {
-        validator: function (value) {
-          return validWinners(value, this.pokemon2.winner);
+    type: {
+      id: {
+        type: Number,
+        required: function (value) {
+          return isSet(value);
         },
-        message: 'There can be only one winner',
       },
+      name: {
+        type: String,
+        required: function (value) {
+          return isSet(value);
+        },
+      },
+      winner: {
+        type: Boolean,
+        required: function (value) {
+          return isSet(value);
+        },
+        validate: {
+          validator: function (value) {
+            return validWinners(value, this.pokemon2.winner);
+          },
+          message: 'There can be only one winner',
+        },
+      },
+    },
+    required: function (value) {
+      return isSet(value);
     },
   },
   pokemon2: {
-    id: {
-      type: Number,
-      required: function (value) {
-        return isSet(value);
-      },
-    },
-    name: {
-      type: String,
-      required: function (value) {
-        return isSet(value);
-      },
-    },
-    winner: {
-      type: Boolean,
-      required: function (value) {
-        return isSet(value);
-      },
-      validate: {
-        validator: function (value) {
-          return validWinners(value, this.pokemon1.winner);
+    type: {
+      id: {
+        type: Number,
+        required: function (value) {
+          return isSet(value);
         },
-        message: 'There can be only one winner',
       },
+      name: {
+        type: String,
+        required: function (value) {
+          return isSet(value);
+        },
+      },
+      winner: {
+        type: Boolean,
+        required: function (value) {
+          return isSet(value);
+        },
+        validate: {
+          validator: function (value) {
+            return validWinners(value, this.pokemon1.winner);
+          },
+          message: 'There can be only one winner',
+        },
+      },
+    },
+    required: function (value) {
+      return isSet(value);
     },
   },
 });
