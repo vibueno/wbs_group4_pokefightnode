80d24668b260255a21bc88ed7458d1972711a295
Víctor IB <20514260+vibueno@users.noreply.github.com>
2021-02-19 15:42:19 +0100

    Pokemon id type declaration in model  (#16)
    * Route "/" get all pokemons
    
    * Add heroku config
    
    * validations
    
    - id > 9 wasn't working properly
    - checked that we get a pokemon from database, otherwise return 404
    
    * Constants for HTTP status codes
    
    * Remove old code
    
    * Improve pokemon schema
    
    - Better data definition
    - Removal of unnecessary arrays
    
    * Add validation if info selected not fetchable
    
    * Shorten paths inside utils folder files
    
    * Add 404 for non existing pages
    
    * Improve buildResponse
    
    * Change id type in pokemon model

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const pokemonSchema = new Schema({
-  id: {
-    type: Number,
-  },
+  id: Number,
   name: {
     english: String,
     japanese: String,
